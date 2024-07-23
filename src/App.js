1b27ee6cec7bff27c6eb7cc5ac03f940cdf7da40
sebastian <smoyano@subcargo.com>
2024-07-23 17:31:43 -0400

    asdasd

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="Pikachu" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> Probando si actualiza.
         </p>
         
       </header>
